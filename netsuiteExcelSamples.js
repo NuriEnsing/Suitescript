ff7eb18b2ae2b4f2625a46bbd9ce18b9d5899ff0
nuriDevWork <n.ensing@corlidogroup.com>
2022-06-23 16:25:04 +0200

    Added netsuiteExcelSamples

---
+++ netsuiteExcelSamples.js
@@ -0,0 +1,60 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType UserEventScript
+ * @NAmdConfig /SuiteScripts/Your Folder//json_conf.json
+ *
+ * @author Nuri Ensing
+ *
+ * @module xlsx
+ *
+ * @description Generate excel files
+ * @references libraries used: SheetJS https://docs.sheetjs.com/docs/example
+ */
+
+define(["N/search", "N/file", "xlsx"], function (search, file, xlsx) {
+    /**
+     * @function createExcelFileWithJsonData
+     * @description Create a Excel file with a named sheet and  populate json data inside the excel file
+     */
+    function createExcelFileWithJsonData() {
+        try {
+            //data which will be populated in the Excel
+            let rows = [
+                { name: "George Washington", birthday: "1732-02-22" },
+                { name: "John Adams", birthday: "1735-10-19" }
+            ];
+            //your folder id where you want the Excel file to be saved
+            const FOLDER_ID = 6799;
+
+            const workbook = xlsx.utils.book_new();
+            const worksheet = xlsx.utils.json_to_sheet(rows);
+
+            //create Excel sheet
+            workbook.SheetNames.push("sheetName");
+            workbook.Sheets["sheetName"] = worksheet;
+
+            let workbookOutput = xlsx.write(workbook, {
+                booktype: "xlsx",
+                type: "base64"
+            });
+
+            let excelFile = file.create({
+                name: "testnuri2.xlsx",
+                fileType: file.Type.EXCEL,
+                contents: workbookOutput,
+                folder: FOLDER_ID
+            });
+
+            return excelFile.save();
+        } catch (e) {
+            log.error({
+                title: e.name,
+                details: e.message
+            });
+        }
+    }
+
+    return {
+        afterSubmit: createExcelFileWithJsonData
+    };
+});
